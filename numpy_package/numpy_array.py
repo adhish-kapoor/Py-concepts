0f714f01be7e62b9761a6e8bfa54261eb2bd7328
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2016-12-21 21:43:10 +0530

    Create numpy_array.py

---
+++ numpy_package/numpy_array.py
@@ -0,0 +1,5 @@
+# The NumPy (Numeric Python) package helps us manipulate large arrays and matrices of numeric data                                  
+import numpy                                              
+a=list(map(float,input().split()))
+print(numpy.array(a,float))           #float is used to print array elements of type float
+                                      #numpy.array() is used to convert a list into a NumPy array
