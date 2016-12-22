052378af8bdabf9492243d38cbcab21697ba2160
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2016-12-22 19:22:54 +0530

    Create String_Validators.py

---
+++ String_Validators.py
@@ -0,0 +1,16 @@
+#string validators are
+
+a.isalnum()        # method to check characters of a string a
+print(a.isalnum()) # returns True if all the characters are alphanumeric(0-9,a-z,A-Z)
+
+a.isalpha()        # method to check characters of a string a
+print(a.isalpha()) # returns True if all the characters are alphabetical(a-z,A-Z)
+
+a.isdigit()        # method to check characters of a string a
+print(a.isdigit()) # returns True if all the characters are digits(0-9)
+
+a.islower()        # method to check characters of a string a
+print(a.islower()) # returns True if all the characters are lowercase(a-z)
+
+a.isupper()        # method to check characters of a string a
+print(a.isupper()) # returns True if all the characters are uppercase(A-Z)
