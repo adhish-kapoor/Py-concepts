1a78b2a4282ae74a25255b152b181e934da43b82
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2018-06-15 12:24:48 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,9 +10,22 @@ print(set("ADHISH"))
 print(len("ADHISH"))           #output 6
 print(len(set("ADHISH")))      #output 5
 
-#itertools.groupby() function
+#itertools.groupby()
 #groupby() generates a break or new group every time the value of the key function changes 
 #(which is why it is usually necessary to have sorted the data using the same key function).
 
 import itertools as it
 print(' '.join(('({}, {})'.format(len(list(g)), x) for x,g in it.groupby(input())))) # input 1222311 #output (1, 1) (3, 2) (1, 3) (2, 1)
+
+#itertools.combinations()
+from itertools import combinations
+a=input()
+s=a.split(' ')
+s[0]=sorted(s[0])
+b=[]
+for i in range(1,int(s[1])+1,1):
+    b.append(list(combinations(s[0],i)))
+
+for i in range(0,len(b),1):
+    for j in range(0,len(b[i]),1):
+        print(*b[i][j],sep="")
